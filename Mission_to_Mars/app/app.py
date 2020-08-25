d6cb35f773949ec24eae78b6ed2d74f52770d02c
redrulz <64673015+redrulz@users.noreply.github.com>
2020-08-24 21:36:27 -0500

    updated python files

--- Mission_to_Mars/app/app.py
+++ Mission_to_Mars/app/app.py
@@ -3,15 +3,16 @@
 
 
 from flask import Flask, render_template, redirect, url_for
-import pymongo as PyMongo
+#import pymongo as PyMongo
+from flask_pymongo import PyMongo
 import scrape_mars
+
 
 app = Flask(__name__)
 
 # Use flask_pymongo to set up mongo connection
 app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_app"
 mongo = PyMongo(app)
-
 
 # Or set inline
 #mongo = PyMongo(app, uri="mongodb://localhost:27017/mars_app")
